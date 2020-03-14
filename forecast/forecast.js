1e6ab40b01a943019d3db8e7c1f18ebc103f4006
Alessandro Baroni <inf.baroni@gmail.com>
2020-03-14 12:42:48 +0100

    Refactoring

--- forecast/forecast.js
+++ forecast/forecast.js
@@ -5,27 +5,35 @@ const regression  = require('regression')
 const csvFile = '../dati-andamento-nazionale/dpc-covid19-ita-andamento-nazionale.csv'
 fs.readFile(csvFile, 'utf8', async function(err, data) {
   const dailyArray = await neatCsv(data)
-  const postive = []
+  const positive = []
   const cured = []
   const dead = []
   const total = []
   dailyArray.forEach((item,i) => {
-    //console.log(item.totale_attualmente_positivi)
-    postive.push([i,parseInt(item.totale_attualmente_positivi)])
-    cured.push([i,item.dimessi_guariti])
+    positive.push([i,parseInt(item.totale_attualmente_positivi)])
     dead.push([i,item.deceduti])
+    cured.push([i,item.dimessi_guariti])
     total.push([i,item.totale_casi])
-
   });
-  let forecast = regression.exponential(postive,{ precision: 16 })
-  console.log(postive)
-  console.log(forecast)
-  console.log(forecast.predict(dailyArray.length))
 
-  let blind = postive.slice(7,dailyArray.length)
-  let blind_forecast = regression.exponential(blind,{ precision: 16 })
-  console.log(blind)
-  console.log(blind_forecast)
-  console.log(blind_forecast.predict(dailyArray.length))
+  const FORECAST_TOMORROW = dailyArray.length
+  const FORECAST_ONE_WEEK = dailyArray.length + 6
+  function forecast(array,start = 1,day = FORECAST_TOMORROW){
+    let blind = array.slice(dailyArray.length - start,dailyArray.length)
+    let blind_forecast = regression.exponential(blind,{ precision: 16 })
+    //console.log(blind)
+    //console.log(blind_forecast)
+    let return_object = {
+      forecast : parseInt(blind_forecast.predict(day)[1]),
+      r2 : blind_forecast.r2
+    }
+    return return_object
 
+  }
+
+  console.log("Positive tomorrow", forecast(positive,3))
+  console.log("Positive next week", forecast(positive,3,FORECAST_ONE_WEEK))
+  console.log("Dead", forecast(dead,3,FORECAST_TOMORROW))
+  //console.log("Cured", forecast(cured,3))
+  //console.log("Total", forecast(total,3))
 })
