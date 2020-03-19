5e707b391d747f2faaa02b2011d7c5d6272917ea
Alessandro Baroni <inf.baroni@gmail.com>
2020-03-19 10:02:37 +0100

    changed output format, delete commented code

--- forecast/forecast.js
+++ forecast/forecast.js
@@ -9,12 +9,14 @@ fs.readFile(csvFile, 'utf8', async function(err, data) {
   const cured = []
   const dead = []
   const total = []
+  const dates= []
   let latest = null
   dailyArray.forEach((item,i) => {
     positive.push([i+1,parseInt(item.totale_attualmente_positivi)])
     dead.push([i+1,parseInt(item.deceduti)])
     cured.push([i+1,parseInt(item.dimessi_guariti)])
     total.push([i+1,parseInt(item.totale_casi)])
+    dates.push(item.data.split(' ')[0])
     latest = item
   });
 
@@ -27,26 +29,16 @@ fs.readFile(csvFile, 'utf8', async function(err, data) {
   ]
   const FORECAST_TOMORROW = dailyArray.length
   const FORECAST_ONE_WEEK = dailyArray.length + 6
-  function forecast(array, latest_to_consider = array.length,day = FORECAST_TOMORROW, method = regression.exponential ){
-    //console.log(latest_to_consider)
-    let blind = array.slice(dailyArray.length - latest_to_consider,dailyArray.length)
-    let blind_forecast = method(blind,{ precision: 16 })
-    //console.log(blind_forecast)
-    let return_object = {
-      forecast : parseInt(blind_forecast.predict(day)[1]),
-      r2 : blind_forecast.r2,
-      string: blind_forecast.string
-    }
-    return return_object
-
-  }
 
   function forecasts(array, latest_to_consider = array.length,day = FORECAST_TOMORROW, method = regression.exponential ){
-    //console.log(latest_to_consider)
-    let blind = array.slice(dailyArray.length - latest_to_consider,dailyArray.length)
+    const startIndex = dailyArray.length - latest_to_consider
+    const endIndex = dailyArray.length
+    const blindDates = dates.map(d => d.split(' ')[0]).slice(startIndex,endIndex)
+    let blind = array.slice(startIndex,endIndex)
     //let blind_forecast = method(blind,{ precision: 16 })
     //console.log(blind_forecast)
     let return_object = {
+      dates : blindDates,
       dataPoints: blind,
       exponential : regression.exponential(blind,{ precision: 16 }),
       linear :  regression.linear(blind,{ precision: 16 }),
@@ -56,45 +48,6 @@ fs.readFile(csvFile, 'utf8', async function(err, data) {
 
   }
 
-  function chooseForecast(data,period = FORECAST_TOMORROW){
-    let best_fc = forecast(data,3,period)
-
-    for(let j=0; j<METHODS.length; j++){
-      for(let i=7; i<= dailyArray.length;i++){
-        let fc = forecast(data,i,period, METHODS[j])
-        //console.log("Positive", i,j, fc)
-        if (fc.r2 > best_fc.r2 && fc.r2 < 1){
-          best_fc = fc
-        }
-      }
-    }
-
-    return best_fc
-  }
-  /*
-  console.log("Tomorrow:")
-  console.log("Positive", chooseForecast(positive))
-  console.log("Dead", chooseForecast(dead))
-  console.log("Cured", chooseForecast(cured))
-  console.log("Total", chooseForecast(total))
-
-  console.log("Next week:")
-  console.log("Positive", chooseForecast(positive,FORECAST_ONE_WEEK))
-  console.log("Dead", chooseForecast(dead,FORECAST_ONE_WEEK))
-  console.log("Cured", chooseForecast(cured,FORECAST_ONE_WEEK))
-  console.log("Total", chooseForecast(total,FORECAST_ONE_WEEK))
-
-  console.log("+++++++++++++")
-  console.log("Positive", forecast(positive,10,FORECAST_ONE_WEEK -3))
-  console.log("Positive", forecast(positive,10,FORECAST_ONE_WEEK -3,regression.power))
-  console.log("Positive", forecast(positive,10,FORECAST_ONE_WEEK -3,regression.polynomial))
-
-  console.log(total.map(e=>e[1]))
-  //console.log(forecast(positive,10,FORECAST_ONE_WEEK -3,regression.polynomial))
-  console.log(forecast(total,total.length,FORECAST_TOMORROW,regression.linear))
-  console.log(forecast(total,total.length,FORECAST_TOMORROW,regression.power))
-  console.log(forecast(total,total.length,FORECAST_TOMORROW,regression.exponential))
-  */
   function getOutputObject(days = total.length){
     const totalForecasts = forecasts(total,days)
     const startingDay = totalForecasts.dataPoints[0][0]
@@ -115,6 +68,7 @@ fs.readFile(csvFile, 'utf8', async function(err, data) {
     }
 
     return {
+      dates: totalForecasts.dates,
       latestDate: latest.data.split(' ')[0],
       total:totalForecasts.dataPoints.map(e=>e[1]),
       dead: dead.map(e=>e[1]),
@@ -131,11 +85,6 @@ fs.readFile(csvFile, 'utf8', async function(err, data) {
       totalR2Exponential: totalForecasts.exponential.r2
     }
   }
-
-  //console.log(getOutputObject())
-  //console.log('*****************************')
-
-  //console.log(getOutputObject(7))
 
   fs.writeFile('allData.json', JSON.stringify(getOutputObject(),null,2) , (err) => {
       if (err) throw err;
